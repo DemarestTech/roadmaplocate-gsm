cc030f3f1f5fb65540c412cc579a4aad1dc0d25f
Ty Demarest <ty@darkbladesystems.com>
2018-07-17 13:16:16 -0400

    fixed arduino sketch

--- server/run.sh
+++ server/run.sh
@@ -14,4 +14,3 @@ docker build -t dtech/rml . \
 
 # To post some data with curl do
 # `curl -d "param1=value1&param2=value2" -X POST http://localhost:80/post`
-# or `curl -d '{"key1":"value1", "key2":"value2"}' -H "Content-Type: application/json" -X POST http://localhost:3000/post`
